2d820a616e55f736a027d432afe20d82f814d042
Sakith-Seneviratne <Sakithsene@gmail.com>
2026-01-22 14:19:36 +0530

    added bentobox2 content

--- app/components/bento-boxes/BentoBox2.tsx
+++ app/components/bento-boxes/BentoBox2.tsx
@@ -3,12 +3,18 @@ import { Globe } from "../Globle";
 
 export default function BentoBox2() {
   return (
-    <div className="h-full w-full rounded-2xl bg-gradient-to-tr from-[#00A9EE] to-white relative overflow-hidden">
-      {/* Box 2 Content - White with light blue gradient from bottom-left */}
-      <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden px-40 pt-8 pb-40 md:pb-60">
-      <Globe className="top-28" />
-    </div>
-
+    <div className="max-h-[520px] w-full rounded-2xl bg-gradient-to-tr from-white via-[#FFFFFF] to-[#00A9EE] relative overflow-hidden">
+      <div className="relative flex flex-col size-full max-w-4xl overflow-hidden pt-6 md:pt-8 pb-40 md:pb-60 px-6 md:px-8">
+        <p className="text-black text-base md:text-3xl font-light mb-4">
+          Our teams have provided services to businesses globally across
+        </p> 
+        <span className="pointer-events-none bg-gradient-to-b from-[#00378A] to-[#00A9EE] bg-clip-text text-left text-5xl md:text-8xl font-light text-transparent tracking-tighter">
+          120+ Countries
+        </span>
+        <div className="flex justify-center -mt-[80px] ">
+          <Globe className="relative top-0" />
+        </div>
+      </div>
     </div>
   );
 }
--- app/components/bento-boxes/BentoBox3.tsx
+++ app/components/bento-boxes/BentoBox3.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BentoBox3() {
   return (
-    <div className="h-[500px] w-full !mt-[100px] rounded-2xl bg-gradient-to-tr from-black to-[#00A9EE] relative overflow-hidden">
+    <div className="h-[600px] w-full mt-[100px] rounded-2xl bg-gradient-to-tr from-black to-[#00A9EE] relative overflow-hidden">
       {/* Box 3 Content - White with light blue gradient from bottom-left, spans 2 rows */}
     </div>
   );
