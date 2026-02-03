8ff750d0684386eac00003cbd86516d700a25913
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-03 14:47:27 +0530

    organize the home

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -50,8 +50,7 @@ export default function Header() {
         animate={{
           width: scrolled ? (isDesktop ? "54%" : "74%") : "100%",
           height: scrolled ? "50px" : "120px",
-          width: navWidth,
-          height: navHeight,
+          
           backdropFilter: scrolled ? "blur(10px)" : "none",
           boxShadow: scrolled
             ? "0 0 24px rgba(245, 245, 245, 0.06), 0 1px 1px rgba(238, 238, 238, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
