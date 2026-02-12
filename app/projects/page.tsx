6fbadff260fbb28a87ca10e86016a8ee5639039e
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-12 16:17:19 +0530

    portfolio

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import { RefreshCcw } from "lucide-react";
+
+export default function ProjectsPage() {
+    return (
+        <div>
+            <h1>Projects    </h1>
+            <RefreshCcw />
+        </div>
+    );
+}
