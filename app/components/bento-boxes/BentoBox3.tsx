5ed1e273a6f90e398cddeb601b788f6e2ae2fc99
Sakith Seneviratne <145918663+Sakith-Seneviratne@users.noreply.github.com>
2026-02-19 11:22:23 +0530

    style: update BentoBox3 component to consolidate text into a single line for improved readability; enhance BentoBox6 component by refactoring logo configuration and implementing smooth scrolling effects with new hooks for better user experience.

--- app/components/bento-boxes/BentoBox3.tsx
+++ app/components/bento-boxes/BentoBox3.tsx
@@ -20,11 +20,9 @@ export default function BentoBox3() {
               120+
             </h2>
             <p className="text-[16px] sm:text-2xl md:text-xl lg:text-3xl xl:text-2xl 2xl:text-3xl tracking-tighter  font-light text-black/90">
-              Successfully Completed
+              Successfully Completed <br></br>Projects
             </p>
-            <p className="text-lg md:text-xl lg:text-2xl font-light text-white/90">
-              Projects
-            </p>
+    xs
           </div>
         </div>
 
