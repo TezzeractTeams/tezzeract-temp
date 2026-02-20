a658ef119baf7e03b29f0efb1c364eddcbc778e0
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-20 11:20:32 +0530

    adding brevo chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Brevo Conversations */}
+        <Script id="brevo-conversations" strategy="afterInteractive">
+          {`
+            (function(d, w, c) {
+              w.BrevoConversationsID = '689ad382f05ff79b4003a4d6';
+              w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+              };
+              var s = d.createElement('script');
+              s.async = true;
+              s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+              if (d.head) d.head.appendChild(s);
+            })(document, window, 'BrevoConversations');
+          `}
+        </Script>
+      </head>
       <body
         className={`${figtree.variable} ${manrope.variable} antialiased`}
         suppressHydrationWarning
